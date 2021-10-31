b472ee5665bee4dec47612acf14bbdcd7d387a2c
Stenbyte <sten.scheifel@gmail.com>
2021-10-31 20:19:27 +0200

    first

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.scss";
+import Main from "../main/Main";
+
+export default function Header() {
+  return (
+    <div>
+      Header
+      <Main />
+    </div>
+  );
+}
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./main.scss";
+
+export default function Main() {
+  return <div>main</div>;
+}
