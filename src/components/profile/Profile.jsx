d3839ee2833426b222870ccb67951a3a1435b794
Stenbyte <sten.scheifel@gmail.com>
2021-11-01 14:00:47 +0200

    profile styling changes

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./profile.module.scss";
-import logo from "../../img/logo.png";
+import logo from "../../img/st.png";
 
 export default function Profile() {
   return (
