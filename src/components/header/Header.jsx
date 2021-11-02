3a84792a4a6c95434c0930f788595c9301e41bf3
Stenbyte <sten.scheifel@gmail.com>
2021-11-02 12:17:58 +0200

    green theme implemented

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,38 +4,41 @@ import Home from "../home/Home";
 
 export default function Header() {
   const [theme, setTheme] = useState("");
-  const handleTheme = () => {
-    theme === "green" ? setTheme("") : setTheme("green");
-  };
-  useEffect(() => {
+
+  const handleTheme = (theme) => {
+    setTheme(theme);
     localStorage.setItem("theme", theme);
-  }, [theme]);
+  };
+
+  useEffect(() => {
+    const current = localStorage.getItem("theme");
+    if (current) {
+      setTheme(current);
+    }
+  }, []);
+
   return (
     <>
       <div className={`header ${theme === "green" ? "head2" : ""}`}>
         <img src="images/reader.svg" alt="" />
         <div className="headerMiddle">
-          <div className={`headerBtn ${theme === "green" ? "btn2" : ""}`}>
-            PROFILE
-          </div>
-          <div className={`headerBtn ${theme === "green" ? "btn2" : ""}`}>
-            FRIENDS
-          </div>
-          <div className={`headerBtn ${theme === "green" ? "btn2" : ""}`}>
-            BOOKS
-          </div>
-          <div className={`headerBtn ${theme === "green" ? "btn2" : ""}`}>
-            NEWS
-          </div>
-          <div className={`headerBtn ${theme === "green" ? "btn2" : ""}`}>
-            MISC
-          </div>
+          <div className={`headerBtn ${theme && "btn2"}`}>PROFILE</div>
+          <div className={`headerBtn ${theme && "btn2"}`}>FRIENDS</div>
+          <div className={`headerBtn ${theme && "btn2"}`}>BOOKS</div>
+          <div className={`headerBtn ${theme && "btn2"}`}>NEWS</div>
+          <div className={`headerBtn ${theme && "btn2"}`}>MISC</div>
         </div>
-        <div
-          className={`headerBtn ${theme === "green" ? "btn2" : ""}`}
-          onClick={handleTheme}
-        >
-          THEME
+        <div className={`headerBtn`}>
+          <p id="" onClick={() => handleTheme("")}>
+            Brown theme
+          </p>
+          <p
+            id="green"
+            onClick={() => handleTheme("green")}
+            style={{ backgroundColor: "#248277" }}
+          >
+            Green theme
+          </p>
         </div>
       </div>
       <Home theme={theme} />
