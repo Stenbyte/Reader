ac46c9703089f278a291d662cab5ed4f63d7460a
Stenbyte <sten.scheifel@gmail.com>
2021-11-02 12:21:03 +0200

    cleaned

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./profile.module.scss";
 import logo from "../../img/st.png";
 
 export default function Profile() {
-  const [color, setColor] = useState("");
-  useEffect(() => {
-    const col = localStorage.getItem("theme");
-    if (col) {
-      setColor(col);
-    }
-  }, []);
-  console.log(color);
   return (
     <div className={styles.container}>
       <div className={styles.userTop}>
