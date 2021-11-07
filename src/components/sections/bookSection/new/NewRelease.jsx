229348f76e3efb9c431b2e77e853b98ee25d955c
Stenbyte <sten.scheifel@gmail.com>
2021-11-07 13:52:29 +0200

    caategory section done

--- src/components/sections/bookSection/new/NewRelease.jsx
+++ src/components/sections/bookSection/new/NewRelease.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import styles from "./newR.module.scss";
 import axios from "axios";
+import ReleaseCard from "./ReleaseCard";
 
 export default function NewRelease() {
   const [cards, setCards] = useState([]);
+  const [category, setCategory] = useState("");
+
   useEffect(() => {
     axios
       .get(
-        "https://www.googleapis.com/books/v1/volumes?q=java&orderBy=newest&maxResults=5&"
+        `https://www.googleapis.com/books/v1/volumes?q=${category}+subject&filter=ebooks&orderBy=newest&maxResults=5&`
       )
       .then((res) => {
         console.log(res.data.items);
@@ -16,15 +19,20 @@ export default function NewRelease() {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
-  // console.log(card.volumeInfo);
+  }, [category]);
+
   return (
     <div className={styles.new}>
       <h4 className={styles.title}>New Releases</h4>
+      <div className={styles.category}>
+        <div onClick={() => setCategory("Education")}>Education</div>
+        <div onClick={() => setCategory("Fiction")}>Fiction</div>
+        <div onClick={() => setCategory("Non-fiction")}>Non-fiction</div>
+      </div>
       {cards.map((card) => (
-        <div className={styles.list} key={card.id}>
-          List of {card.volumeInfo.title}
-        </div>
+        <>
+          <ReleaseCard card={card} />
+        </>
       ))}
     </div>
   );
---
+++ src/components/sections/bookSection/new/ReleaseCard.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import styles from "./Rel.module.scss";
+import book from "../../../../img/book.png";
+
+export default function ReleaseCard({ card }) {
+  const [modal, setModal] = useState(false);
+  const handleModal = () => {
+    setModal(!modal);
+  };
+  //   console.log(card);
+  return (
+    <>
+      <div className={styles.list} key={card.id} onClick={handleModal}>
+        <img
+          src={
+            card.volumeInfo.imageLinks
+              ? card.volumeInfo.imageLinks.smallThumbnail
+              : book
+          }
+          alt=""
+        />
+        <div className={styles.listInfo}>
+          <h4> {card.volumeInfo.authors}</h4>
+          {card.volumeInfo.title}
+        </div>
+      </div>
+
+      {modal && (
+        <div className={styles.modal} onClick={handleModal}>
+          <div className={styles.card}>
+            <div className={styles.cardTop}>
+              <img
+                src={
+                  card.volumeInfo.imageLinks
+                    ? card.volumeInfo.imageLinks.smallThumbnail
+                    : book
+                }
+                alt=""
+              />
+              <div className={styles.cardBottom}>
+                <h4>Author :{card.volumeInfo?.authors}</h4>
+                <p>Category : {card.volumeInfo?.categories}</p>
+                <p>
+                  <a
+                    href={card.volumeInfo?.infoLink}
+                    alt=""
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Info
+                  </a>
+                  <a
+                    href={card.volumeInfo?.previewLink}
+                    alt=""
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Preview
+                  </a>
+                </p>
+                <p>Language : {card.volumeInfo?.language.toUpperCase()}</p>
+                <p>Pages : {card.volumeInfo?.pageCount}</p>
+                <p>Type : {card.volumeInfo?.printType}</p>
+                <p>Date : {card.volumeInfo?.publishedDate}</p>
+                <p>
+                  Price : {card.saleInfo.listPrice?.amount}
+                  {card.saleInfo.listPrice?.currencyCode}
+                </p>
+              </div>
+            </div>
+            <div className={styles.description}>
+              {card.volumeInfo.description}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
