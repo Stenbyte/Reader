f91e0b820351b3cb2cc3f9a59c8d1395c41e2a60
Stenbyte <sten.scheifel@gmail.com>
2021-11-10 17:06:00 +0200

    dictionary api first step

--- src/components/sections/shelfSection/Bookshelf.jsx
+++ src/components/sections/shelfSection/Bookshelf.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./misc.module.scss";
+import axios from "axios";
 
 export default function MisSec({ theme }) {
+  let API = process.env.REACT_APP_API_KEY;
+  console.log(process.env);
+  const [word, setWord] = useState("");
+  const [newWord, setNewWord] = useState("");
+  const wordHandler = (e) => {
+    setWord(e.target.value);
+  };
+  // console.log(word);
+  const enterHandle = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(
+          `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${API}`
+        )
+        .then((res) => {
+          console.log(res.data[0]);
+          setNewWord(res.data[0]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
   return (
     <div
       className={`${styles.container} ${theme === "" ? "" : `${styles.hom1}`}`}
@@ -18,8 +42,25 @@ export default function MisSec({ theme }) {
           </div>
         </div>
         <div className={styles.right}>
-          <div className={styles.rightTop}>
+          <div className={styles.rightTop} onClick={wordHandler}>
             <h3>Dictionary</h3>
+          </div>
+          <div className={styles.dic}>
+            <input type="text" onChange={wordHandler} onKeyDown={enterHandle} />
+            <span>Save Word</span>
+            <div className={styles.description}>
+              <h2>
+                {newWord.hwi?.hw.toUpperCase()} <span>{newWord.fl}</span>
+              </h2>
+              <h4>Deffiniton</h4>
+              <p>{newWord.fl}</p>
+              <p>
+                {/* Offensive: {newWord.meta?.offensive === false ? "No" : "Yes"}{" "} */}
+              </p>
+              <p>1: {newWord.shortdef ? newWord.shortdef[0] : ""} </p>
+              <p>2: {newWord.shortdef ? newWord.shortdef[1] : ""} </p>
+              <p>3: {newWord.shortdef ? newWord.shortdef[2] : ""} </p>
+            </div>
           </div>
         </div>
       </div>
