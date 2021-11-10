ea6218bf8dcf201a89dd1a30a5c64b6c906c6ca4
Stenbyte <sten.scheifel@gmail.com>
2021-11-10 17:45:05 +0200

    dictionary styling, error, loading handle

--- src/components/sections/shelfSection/Bookshelf.jsx
+++ src/components/sections/shelfSection/Bookshelf.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./misc.module.scss";
+import styles from "./shelf.module.scss";
 import axios from "axios";
 
 export default function MisSec({ theme }) {
@@ -7,22 +7,28 @@ export default function MisSec({ theme }) {
   console.log(process.env);
   const [word, setWord] = useState("");
   const [newWord, setNewWord] = useState("");
+  const [load, setLoad] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const wordHandler = (e) => {
     setWord(e.target.value);
   };
-  // console.log(word);
   const enterHandle = (e) => {
     if (e.key === "Enter") {
+      setLoading(true);
       axios
         .get(
           `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${API}`
         )
         .then((res) => {
-          console.log(res.data[0]);
           setNewWord(res.data[0]);
+          setLoad(true);
+          setLoading(false);
         })
         .catch((err) => {
           console.log(err);
+          setError(err.message);
         });
     }
   };
@@ -46,20 +52,31 @@ export default function MisSec({ theme }) {
             <h3>Dictionary</h3>
           </div>
           <div className={styles.dic}>
-            <input type="text" onChange={wordHandler} onKeyDown={enterHandle} />
-            <span>Save Word</span>
+            <input
+              type="text"
+              onChange={wordHandler}
+              onKeyDown={enterHandle}
+              placeholder="Search for a Word"
+            />
+            <span className={styles.span}>Save Word</span>
             <div className={styles.description}>
               <h2>
                 {newWord.hwi?.hw.toUpperCase()} <span>{newWord.fl}</span>
               </h2>
-              <h4>Deffiniton</h4>
-              <p>{newWord.fl}</p>
-              <p>
-                {/* Offensive: {newWord.meta?.offensive === false ? "No" : "Yes"}{" "} */}
-              </p>
-              <p>1: {newWord.shortdef ? newWord.shortdef[0] : ""} </p>
-              <p>2: {newWord.shortdef ? newWord.shortdef[1] : ""} </p>
-              <p>3: {newWord.shortdef ? newWord.shortdef[2] : ""} </p>
+              {loading && <p>Loading...</p>}
+              {load && (
+                <>
+                  <h4>Deffiniton</h4>
+                  <p>
+                    Offensive:{" "}
+                    {newWord.meta?.offensive === false ? "No" : "Yes"}
+                  </p>
+                  <p>1: {newWord.shortdef ? newWord.shortdef[0] : ""} </p>
+                  <p>2: {newWord.shortdef ? newWord.shortdef[1] : ""} </p>
+                  <p>3: {newWord.shortdef ? newWord.shortdef[2] : ""} </p>
+                </>
+              )}
+              {error}
             </div>
           </div>
         </div>
