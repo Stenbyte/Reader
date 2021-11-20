329753b8c10aedc02600d5b4695ed73c19183373
Stenbyte <sten.scheifel@gmail.com>
2021-11-20 21:21:38 +0200

    error handling

--- src/components/sections/newsSection/NewsSec.jsx
+++ src/components/sections/newsSection/NewsSec.jsx
@@ -6,8 +6,10 @@ import Article from "./Article";
 export default function NewsSec({ theme }) {
   const [post, setPost] = useState([]);
   const [error, setError] = useState("");
+  const [load, setLoad] = useState(false);
   const apiKey = process.env.REACT_APP_NYT_KEY;
   useEffect(() => {
+    setLoad(true);
     axios
       .get(
         `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=books&api-key=${apiKey}`
@@ -15,19 +17,20 @@ export default function NewsSec({ theme }) {
       .then((res) => {
         // res.data;
         setPost(res.data.response.docs);
-        // console.log(res.data.response.docs);
+        setLoad(false);
       })
       .catch((err) => {
         // console.log(err.message);
         setError(err.message);
-      }, []);
-  });
+      });
+  }, [apiKey]);
 
   return (
     <div
       className={`${styles.container} ${theme === "" ? "" : `${styles.hom1}`}`}
     >
       <div className={styles.card}>
+        {load && <p>Loading...</p>}
         {post.map((article) => (
           <Article key={article._id} article={article} />
         ))}
